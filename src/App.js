fa853d07d008b73c5423c5a87645f6be0bd19f47
eoin-barr <eoin.barr@ucdconnect.ie>
2021-09-10 18:15:56 +0100

    base home page style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import { getAllPosts } from './lib/api'
+// import { getAllPosts } from './lib/api'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Posts from './components/common/Posts'
 
 function App() {
 
-  React.useEffect(() => {
-    const getData = async () => {
-      const res = await getAllPosts()
-      console.log(res.data)
-    }
-    getData()
-  })
 
-  return <h1>Hello World</h1>
+  // React.useEffect(() => {
+  //   const getData = async () => {
+  //     const res = await getAllPosts()
+  //     console.log(res.data)
+  //   }
+  //   getData()
+  // })
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/posts" component={Posts} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/components/posts/StandardPostCard.js
@@ -0,0 +1,12 @@
+
+
+function StandardPostCard({ post }) {
+
+  return (
+    <div className="testing">
+      <h1 className="testing">{post.title}</h1>
+    </div>
+  )
+}
+
+export default StandardPostCard
